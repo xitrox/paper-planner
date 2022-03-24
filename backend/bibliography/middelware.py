eae6b7bfe7d0531c8de9c7b54be4c0dfb4c8fb74
abgestoerzt <79547235+abgestoerzt@users.noreply.github.com>
2022-03-24 19:31:05 +0100

    btn

--- backend/bibliography/middelware.py
+++ backend/bibliography/middelware.py
@@ -11,6 +11,6 @@ class MyMiddle(MiddlewareMixin):
     def process_response(self, request, response):
         if request.method == 'OPTIONS':
             response['Access-Control-Allow-Methods'] = CORS['Access-Control-Allow-Methods']
-        response['Access-Control-Allow-Headers'] = CORS['Access-Control-Allow-Headers']
-        response['Access-Control-Allow-Origin'] = CORS['Access-Control-Allow-Origin']
-        return response
+            response['Access-Control-Allow-Headers'] = CORS['Access-Control-Allow-Headers']
+            response['Access-Control-Allow-Origin'] = CORS['Access-Control-Allow-Origin']
+            return response
